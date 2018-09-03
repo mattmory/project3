f6885c18af3ac6b393986e64755d6b42b899ee01
Andrew <andrewbowden92@gmail.com>
2018-09-03 16:31:53 -0700

    nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,15 +5,15 @@ const Nav = () => (
   <nav className="container-fluid navbar navbar-expand-lg row">
     <section className="col-sm-2">
       <a className="navbar-brand" id="logo" href="/">  </a>
-        <img src="/src/images/logo.png" alt="Placeholder"></img>
+      <h1 id="logo">Cocktail Creator</h1>
     </section>
-    <section className="col-sm-6 navbar ">
+    <section className="col-sm-4 navbar ">
           <li className="nav-link">
-                        <a href="Ingredients.html " className="active "  role="button " data-toggle="button">Bio</a></li>
+                        <a href="Ingredients.html " className="active "  role="button " data-toggle="button">Ingredients</a></li>
           <li className="nav-link ">
                         <a href="Recipes.html "  role="button" data-toggle="button">Recipes</a></li>
           <li className="nav-link ">
-                        <a href="Goodtimes.html " role="button" data-toggle="button">Goodtimes</a></li>
+                        <a href="Goodtimes.html " role="button" data-toggle="button">Favorites</a></li>
       </section>
       <ul className="navbar-nav mr-auto"></ul>
       <section className="col-sm-2">
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Home.css";
 import Login from "../../components/Login";
-
+import Nav from "../../components/Nav";
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <Nav />
         <Login />
       </div>
     );
