25dac09fa40cadbcd3151462bf6ba2ee1cb89196
Andrew <andrewbowden92@gmail.com>
2018-09-03 16:51:42 -0700

    nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,19 +1,18 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = () => (
 <div id="navLinks">
   <nav className="container-fluid navbar navbar-expand-lg row">
-    <section className="col-sm-2">
+    <section className="col-sm-4">
       <a className="navbar-brand" id="logo" href="/">  </a>
-      <h1 id="logo">Cocktail Creator</h1>
+      <h1 id="logo"> Cocktail </h1> <h1 id="logoTwo">Creator </h1>
     </section>
-    <section className="col-sm-4 navbar ">
+    <section className="col-m-3 navbar center ">
           <li className="nav-link">
-                        <a href="Ingredients.html " className="active "  role="button " data-toggle="button">Ingredients</a></li>
+                        <a href="Ingredients.html " className="active "  role="button " data-toggle="button">ingredients</a></li>
           <li className="nav-link ">
-                        <a href="Recipes.html "  role="button" data-toggle="button">Recipes</a></li>
-          <li className="nav-link ">
-                        <a href="Goodtimes.html " role="button" data-toggle="button">Favorites</a></li>
+                        <a href="Recipes.html "  role="button" data-toggle="button">recipes</a></li>
       </section>
       <ul className="navbar-nav mr-auto"></ul>
       <section className="col-sm-2">
