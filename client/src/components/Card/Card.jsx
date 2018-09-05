206065008569ada8c1432a335bfae475edb60346
Andrew <andrewbowden92@gmail.com>
2018-09-05 13:19:36 -0700

    Card Component

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Style.css";
+import { Container, Row, Col } from "../Grid";
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="card" onClick={() => props.setClicked(props.id)}>
+        <div className="img-container">
+          <img alt={props.name} src={props.image} />
+        </div>
+        <div className="content">
+          {props.name}
+          {props.ingredients}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
+
+// export const RecipeListItem = props => (
+//     <li className="list-group-item">
+//       <Container>
+//         <Row>
+//           <Col size="xs-4 sm-2">
+//             <Thumbnail src={props.thumbnail || "https://placehold.it/300x300"} />
+//           </Col>
+//           <Col size="xs-8 sm-9">
+//             <h3>{props.title}</h3>
+//             <p>Ingredients: {props.ingredients}</p>
+//             <a rel="noreferrer noopener" target="_blank" href={props.href}>
+//               Go to recipe!
+//             </a>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </li>
+//   );
