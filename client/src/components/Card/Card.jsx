b70b806fc67aa01b03a4824176255dd1addfe1d2
L-Ward <leahobney@gmail.com>
2018-09-07 07:57:57 -0700

    add icon to card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Card.css";
 import { Container, Row, Col } from "../Grid";
 import API from "../../utils/API";
+import DrinkIcon from "../DrinkIcon";
 
 
 class Card extends React.Component {
@@ -27,15 +28,21 @@ class Card extends React.Component {
   render() {
     return (
       this.state.drinkData !== null ? (
-        <div className="col-4">
+        <div className="col-sm-12 col-md-6 col-lg-4">
           <div className="card rounded">
             <img className="card-image-top rounded-top" alt={this.state.drinkData.drinkName} src={this.state.drinkData.thumbImg} />
-            <div className="content">
-              <h1 className="drinkName">{this.state.drinkData.drinkName}</h1>
-              {this.state.drinkData.contents.map(Ing => (
-                <div>{Ing.ingredientName}: {Ing.ingredientAmount}</div>
-              ))}
-              {this.state.drinkData.instructions}
+            <div className="card-body">
+              <h1 className="drinkName">{this.state.drinkData.drinkName}
+                <span className="drink-icon-card float-right">
+                  <DrinkIcon />
+                </span>
+              </h1>
+              <div className="card-text">
+                {this.state.drinkData.contents.map(Ing => (
+                  <div>{Ing.ingredientName}: {Ing.ingredientAmount}</div>
+                ))}
+                <p className="cardInstructions">{this.state.drinkData.instructions}</p>
+              </div>
             </div>
           </div>
         </div>
